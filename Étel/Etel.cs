84da22a0483af5781bd5295b8588bc8a4dc52d7e
Petrik Diak <diak@petrik.hu>
2025-04-04 12:30:37 +0200

    Etel osztaly kesz

---
+++ Étel/Etel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Étel
+{
+	internal abstract class Etel
+	{
+		double caloriesPerDkg;
+		double weight;
+		string name;
+		public bool IsItDairyFree { get; set; }
+		public double CaloriesPerDkg { get => caloriesPerDkg; set => caloriesPerDkg = value; }
+		public double Weight { get => weight; set => weight = value; }
+		public string Name { get => name; set => name = value; }
+
+		public abstract double CountCalories();
+		
+		
+
+		protected Etel(double caloriesPerDkg, double weight, string name)
+		{
+				this.caloriesPerDkg = caloriesPerDkg;
+				this.weight = weight;
+				this.name = name;
+				IsItDairyFree = false;
+		}
+	}
+}
